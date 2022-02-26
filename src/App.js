47986217ea8c8ea4df250faea5a220a30300e2a1
Emmanuel <enobakhare.emmanuel@gmail.com>
2022-02-26 23:27:42 +0100

    added the second filter

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { useState } from "react";
 export default function App() {
   const [filterName, setFilterName] = useState(false);
   const [filterDuration, setFilterDuration] = useState(false);
+  const [ascending, setAscending] = useState(false);
+  const [descending, setDescending] = useState(false)
 
   const handleChange = (e) => {
     let value = e.target.value;
     console.log(value);
+    setAscending(true)
     if (value === "name") {
       setFilterName(true);
     } else {
@@ -20,6 +23,25 @@ export default function App() {
       setFilterDuration(false);
     }
   };
+
+  const handleSort = (e) => {
+    let sort = e.target.value;
+    console.log(sort);
+    if (sort === "ascending") {
+      setAscending(true);
+    } else {
+      setAscending(false);
+    }
+    if (sort === "descending") {
+      setDescending(true);
+    } else {
+      setDescending(false);
+    }
+    if (sort === "null") {
+      setDescending(false);
+      setAscending(false);
+    }
+  };
   return (
     <div className="App">
       <div>
@@ -28,29 +50,45 @@ export default function App() {
           <option value="name">Name</option>
           <option value="duration">Duration</option>
         </select>
-        <select className="sort" onChange={handleChange}>
-          <option>All</option>
+        <select className="sort" onChange={handleSort}>
+          <option value='null'>All</option>
           <option value="ascending">Ascending Order</option>
           <option value="descending">Descending Order</option>
         </select>
       </div>
-      {filterName && Song ? (
+      {filterName && Song && ascending ? (
         <>
-          {Song.map((song) => (
+          {Song.sort((a,b) => a.name > b.name ? 1 : -1).map((song) => (
             <h3>{song.name}</h3>
           ))}
         </>
-      ) : filterDuration && Song ? (
+      ) : filterName && Song && descending ? (
         <>
-          {Song.map((song) => (
-            <h3>{song.duration}</h3>
+          {Song.sort((a,b) => a.name < b.name ? 1 : -1).map((song) => (
+            <h3>{song.name}</h3>
           ))}
         </>
-      ) : (
+      ) 
+      :
+      filterDuration && Song && ascending ? (
+        <>
+          {Song.sort((a,b) => a.duration - b.duration).map((song) => (
+            <p>Duration: {song.duration}</p>
+          ))}
+        </>
+      ) : filterDuration && Song && descending ? (
+        <>
+          {Song.sort((a,b) => b.duration - a.duration).map((song) => (
+            <p>Duration: {song.duration}</p>
+          ))}
+        </>
+      )
+      :
+      (
         Song.map((song, i) => (
           <div key={i}>
             <h3>{song.name}</h3>
-            <p>{song.duration}</p>
+            <p>Duration: {song.duration}</p>
           </div>
         ))
       )}
