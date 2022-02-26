de1e9c30a4083cdfed3e20948617ca1e1b608004
Emmanuel <enobakhare.emmanuel@gmail.com>
2022-02-26 21:35:48 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,59 @@
-import "./styles.css";
-import {Song} from './songs'
+import "./App.css";
+import { Song } from "./songs";
 import { useState } from "react";
 
 export default function App() {
-const [filterName, setFilterName] = useState(false)
-const [filterDuration, setFilterDuration] = useState(false)
+  const [filterName, setFilterName] = useState(false);
+  const [filterDuration, setFilterDuration] = useState(false);
 
-const handleChange = (e) => {
-  let value = e.target.value
-  console.log(value)
-  if(value === 'name') {
-    setFilterName(true);
-  } else {setFilterName(false);}
-  if(value === 'duration') {
-    setFilterDuration(true);
-  } else {setFilterDuration(false)}
-}
-return (
+  const handleChange = (e) => {
+    let value = e.target.value;
+    console.log(value);
+    if (value === "name") {
+      setFilterName(true);
+    } else {
+      setFilterName(false);
+    }
+    if (value === "duration") {
+      setFilterDuration(true);
+    } else {
+      setFilterDuration(false);
+    }
+  };
+  return (
     <div className="App">
       <div>
         <select onChange={handleChange}>
           <option>All</option>
-          <option value='name'>Name</option>
-          <option value='duration'>Duration</option>
+          <option value="name">Name</option>
+          <option value="duration">Duration</option>
+        </select>
+        <select className="sort" onChange={handleChange}>
+          <option>All</option>
+          <option value="ascending">Ascending Order</option>
+          <option value="descending">Descending Order</option>
         </select>
       </div>
-      {(filterName && Song) ? 
-      <>
-      {
-        Song.map(song => 
-        <h3>{song.name}</h3>
-        )}
-      </> 
-      : 
-      (filterDuration && Song) ? 
+      {filterName && Song ? (
         <>
-        {
-          Song.map(song => 
-          <h3>{song.duration}</h3>
-          )}
-        </> 
-        :
-      Song.map((song,i) => 
-        <div key={i}>
-          <h3>{song.name}</h3>
-          <p>{song.duration}</p>
-        </div> 
-         )
-      }
-
-      
-   
-    
+          {Song.map((song) => (
+            <h3>{song.name}</h3>
+          ))}
+        </>
+      ) : filterDuration && Song ? (
+        <>
+          {Song.map((song) => (
+            <h3>{song.duration}</h3>
+          ))}
+        </>
+      ) : (
+        Song.map((song, i) => (
+          <div key={i}>
+            <h3>{song.name}</h3>
+            <p>{song.duration}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
